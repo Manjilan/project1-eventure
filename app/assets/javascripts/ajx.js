b29ca6c74df3d3ce5cc0b049c2f81cf99bd3abf6
Manjilan <nakarmimanjila@yahoo.com>
2018-04-18 10:05:06 -0700

    fixed ajax call

--- app/assets/javascripts/ajx.js
+++ app/assets/javascripts/ajx.js
@@ -1,23 +1,16 @@
 // var token = 'BA5XEL577APPWGLN4FNE3SDQLOW7A7ITGKHAVE7ULXYIQGK6GZ';
-
 // var token = 'GGAQ2BUKIRGJMZMU55YZ';
 var token = 'DQVX342GH577YJBOWRDB';
-
 var fullURL = "https://www.eventbriteapi.com/v3/events/search/?token=DQVX342GH577YJBOWRDB"
 var barca = "https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.address=barcelona%2C+spain&start_date.keyword=this_week&token=DQVX342GH577YJBOWRDB"
 var sf = "https://www.eventbriteapi.com/v3/events/search/?sort_by=distance&location.address=san+francisco&start_date.keyword=this_week&token="+ token
-
 //Halim tab idea
-var tab=[]
-
+var tab=[];
 $.ajax({
   method: "GET",
   url: sf,
-
   success: function(json) {
-
     displayEvents(json);
-
   },
   error: function() {
     alert("There was an error getting event data.");
@@ -25,22 +18,65 @@ $.ajax({
   beforeSend: function() {
     $("#page").append("Loading");
   },
-  complete: function() {
+  compvare: function() {
     $("#loading").remove();
   }
 });
-
-
-let displayEvents = function(json) {
-
+var allEeventsList = [];
+var displayEvents = function(json) {
   tab = json.events;
-
   json.events.forEach(function(event, i) {
-  // console.log(event);
-
+    // allEeventsList.push(event);
+    save(event);
+  console.log(event.name.text);
       var eventInstance = "<div class='event-section'><div class='event_item'><div class='row event-row'><p class='title sidebyside'> "+ event.name.text +"</p></div></div></div>";
-
-    $("#info").append(eventInstance);
+      $("#info").append(eventInstance);
   });
-
+}; //end displayEvents
+// events_list = [];
+// function save(index){
+//   console.log(tab[index].description.text);
+//   events_list.push(tab[index]);
+//
+//
+//
+//   //Halim internal AJAX
+//   $.ajax({
+//     url: '/events',
+//     method:'POST',
+//     data: {
+//     event_name: tab[index].name.text,
+//     event_description: tab[index].description.text
+//
+//     },
+//     success: function(data){
+//       $('#event_list').append(<p class="sidebyside">+'data.event_name'+ </p> <button class="btn btn-floating sidebyside">X</button>)
+//     },
+//     error:function(){
+//       alert('not added')
+//     }
+//   });
+//
+// };
+//
+//
+function save(event){
+ console.log(event);
+  $.ajax({
+    url: '/events',
+    method:'POST',
+    data: {
+        event_name: event.name.text,
+        event_description: event.description.text
+        },
+    success: function(data){
+      // $('#event_list').append(<p class="sidebyside">+'data.event_name'+</p> <button class="btn btn-floating sidebyside">+</button>)
+      $('#event_list').append(data);
+    },
+    error:function(a, b, c){
+      console.log(a);
+      console.log(b);
+      console.log(c);
+    }
+  });
 };
